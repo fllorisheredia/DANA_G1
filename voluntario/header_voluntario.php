99d702130a54aabea4035db95c226702356c2cc3
maxiboca <tevesmaxi971@gmail.com>
2025-04-22 16:24:35 +0200

    arreglo

--- voluntario/header_voluntario.php
+++ voluntario/header_voluntario.php
@@ -25,7 +25,7 @@
 
         <div class="navbar-center">
             <ul class="menu menu-horizontal px-4 space-x-6">
-                <li><a href="subir_producto.php" class="text-md text-black hover:bg-purple-600 font-semibold">Inicio</a>
+                <li><a href="index_voluntario.php" class="text-md text-black hover:bg-purple-600 font-semibold">Inicio</a>
                 </li>
                 <li><a href="quienes_somos_voluntario.php"
                         class="text-md text-black hover:bg-purple-600 font-semibold">Quiénes
@@ -36,7 +36,7 @@
         </div>
 
         <div class="navbar-end gap-2">
-            <a href="subir_producto.php" class="btn btn-outline btn-sm hover:bg-purple-600 text-black">Inicio </a>
+            <a href="subir_producto.php" class="btn btn-outline btn-sm hover:bg-purple-600 text-black">Subir Producto </a>
             <a href="perfilUsuario.php"
                 class="btn btn-sm bg-violet-700 hover:bg-violet-900 text-black">Perfil</a>
         </div>
